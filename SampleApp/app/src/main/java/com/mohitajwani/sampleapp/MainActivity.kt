d83fd9ee653ef6c25d056cae02ca5e53094094fa
Mohit Ajwani <mohitajwani@gmail.com>
2018-01-06 23:58:07 +0530

    Optimise imports

--- SampleApp/app/src/main/java/com/mohitajwani/sampleapp/MainActivity.kt
+++ SampleApp/app/src/main/java/com/mohitajwani/sampleapp/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.mohitajwani.sampleapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.google.gson.GsonBuilder
