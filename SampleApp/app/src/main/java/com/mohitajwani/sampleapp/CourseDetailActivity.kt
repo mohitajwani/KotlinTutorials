3e0e856d5c33027787110039d7c9c989442eafa6
Mohit Ajwani <mohitajwani@gmail.com>
2018-01-08 01:19:52 +0530

    Made new layout for Course Detail

--- SampleApp/app/src/main/java/com/mohitajwani/sampleapp/CourseDetailActivity.kt
+++ SampleApp/app/src/main/java/com/mohitajwani/sampleapp/CourseDetailActivity.kt
@@ -1,10 +1,19 @@
 package com.mohitajwani.sampleapp
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_course_detail.*
 
 /**
  * Created by Mohit Ajwani.
  */
 class CourseDetailActivity: AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_detail)
+
+        recyclerView_courseDetail.layoutManager = LinearLayoutManager(this)
+    }
 }
