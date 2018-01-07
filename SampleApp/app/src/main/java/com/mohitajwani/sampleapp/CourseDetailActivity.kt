63673d7d2597ee3e5bd91c8068f6ecd10d75eb76
Mohit Ajwani <mohitajwani@gmail.com>
2018-01-08 01:37:27 +0530

    Added row for Course Lesson

--- SampleApp/app/src/main/java/com/mohitajwani/sampleapp/CourseDetailActivity.kt
+++ SampleApp/app/src/main/java/com/mohitajwani/sampleapp/CourseDetailActivity.kt
@@ -3,6 +3,10 @@ package com.mohitajwani.sampleapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_course_detail.*
 
 /**
@@ -15,5 +19,27 @@ class CourseDetailActivity: AppCompatActivity() {
         setContentView(R.layout.activity_course_detail)
 
         recyclerView_courseDetail.layoutManager = LinearLayoutManager(this)
+    }
+
+    private class CourseDetailAdapter: RecyclerView.Adapter<CourseLessonViewHolder>() {
+
+        override fun onBindViewHolder(holder: CourseLessonViewHolder?, position: Int) {
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CourseLessonViewHolder {
+            val layoutInflater = LayoutInflater.from(parent?.context)
+            val customView = layoutInflater.inflate(R.layout.video_row, parent, false)
+            return CourseLessonViewHolder(customView)
+        }
+
+        override fun getItemCount(): Int {
+            return 5
+        }
+
+    }
+
+    private class CourseLessonViewHolder(val customView: View): RecyclerView.ViewHolder(customView){
+
     }
 }
